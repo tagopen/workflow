6452f58034edb7d0c3f13a7bac4f06bd060b4e49
Security2431 <Artem2431@gmail.com>
2016-09-06 08:08:03 +0300

    fix message value contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,7 +13,7 @@ $(function() {
             var name = $form.find("input[name=name]").val();
             var email = $form.find("input[name=email]").val();
             var phone = $form.find("input[name=phone]").val();
-            var message = $form.find("input[name=message]").val();
+            var message = $form.find("[name=message]").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
