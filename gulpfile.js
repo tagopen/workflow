a5fe498ccb47446d76ebe3f84e99aa5e712754c9
Security2431 <Artem2431@gmail.com>
2017-01-08 19:40:05 +0200

    add bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp          = require('gulp'),
-    sass          = require('gulp-compass'),
+    sass          = require('gulp-sass'),
     browserSync   = require('browser-sync'),
     concat        = require('gulp-concat'),
     uglify        = require('gulp-uglifyjs'),
-    critical      = require('critical').stream,
     cssnano       = require('gulp-cssnano'),
     rename        = require('gulp-rename'),
     del           = require('del'),
     imagemin      = require('gulp-imagemin'),
     pngquant      = require('imagemin-pngquant'),
     cache         = require('gulp-cache'),
-    autoprefixer  = require('gulp-autoprefixer');
+    autoprefixer  = require('gulp-autoprefixer'),
+    bower         = require('gulp-bower'),
+    critical      = require('critical').stream;
 
 gulp.task('sass', function() {
   return gulp.src(['app/sass/**/*.+(scss|sass)'])
@@ -19,24 +20,22 @@ gulp.task('sass', function() {
       sass: 'app/sass',
       image: 'app/img'
     }))
-    .on('error', function(error) {
-      // Would like to catch the error here 
-      console.log(error);
-      this.emit('end');
-    })
     .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 9'], {cascade: true}))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', ['bower'], function() {
   return gulp.src([
-      'app/bower_components/jquery/dist/jquery.min.js',
-      'app/bower_components/magnific-popup/dist/jquery.magnific-popup.min.js'
+      'app/libs/jquery/dist/jquery.js'
     ])
-  .pipe(concat('libs.min.js'))
-  .pipe(uglify())
+  //.pipe(concat('libs.js'))
+  //.pipe(uglify())
   .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('bower', function() {
+  return bower();
 });
 
 gulp.task('css-libs', ['sass'], function() {
@@ -89,18 +88,32 @@ gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
 
 gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
   var buildCss = gulp.src([
-      'app/css/style.css',
-      'app/css/libs.min.css',
+      'app/css/**/*.css'
     ])
     .pipe(gulp.dest('dist/css'));
 
-  var buildFonts = gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'));
+  var buildFonts = gulp.src('app/font/**/*')
+    .pipe(gulp.dest('dist/font'));
 
   var buildJs = gulp.src('app/js/**/*')
     .pipe(gulp.dest('dist/js'));
 
   var buildHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist'));
+});
 
+gulp.task('buildMin', ['clean', 'img', 'sass', 'scripts'], function() {
+  var buildCss = gulp.src([
+      'app/css/**/*.css'
+    ])
+    .pipe(gulp.dest('dist/css'));
+
+  var buildFonts = gulp.src('app/font/**/*')
+    .pipe(gulp.dest('dist/font'));
+
+  var buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+  var buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
 });
