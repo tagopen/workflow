7206dcde9fe3591814db80093aaf7b61fc972aaa
Security2431 <Artem2431@gmail.com>
2017-08-28 00:47:49 +0300

    update mail.php

--- app/js/contact_me.js
+++ app/js/contact_me.js
@@ -1,45 +1,70 @@
 $(function() {
-
-  $(".contactForm input, .contactForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
+  $('.contactForm').validator().on('submit', function (e) {
+    var $form = $(this);
+    if (e.isDefaultPrevented()) {
+      // handle the invalid form...
+    } else {
+      e.preventDefault();
       $form.find("[type=submit]").prop("disabled", true).button('loading'); //prevent submit behaviour and display preloading
-      
-      // get values from FORM
-      var form    = $form.find('[type=submit]').attr("name"),
-          name    = $form.find("[name=name]").val(),
-          email   = $form.find("[name=email]").val(),
-          phone   = $form.find("[name=phone]").val(),
-          message = $form.find("[name=message]").val();
+
+       // get values from FORM
+      var form               = $form.find('[type=submit]').val(),
+          name               = $form.find('[name=name]').val(),
+          email              = $form.find('[name=email]').val(),
+          phone              = $form.find('[name=phone]').val(),
+          message            = $form.find('[name=message]').val(),
+          time               = $form.find('[name=time]').val(),
+          method             = $form.find('[name=method]').val(),
+          range1             = $form.find('[name=range1]').val(),
+          range2             = $form.find('[name=range2]').val(),
+          period             = new Array(),
+          material           = new Array();
+
+      $form.find("[name^=\"period\"]:checked").each(function() {
+        if ($(this).prop("checked")) {
+          var radioText = $(this).siblings().text();
+
+          period.push($.trim(radioText));
+        }
+      });
+
+      $form.find("[name^=\"material\"]:checked").each(function() {
+        if ($(this).prop("checked")) {
+          var radioText = $(this).siblings().text();
+
+          material.push($.trim(radioText));
+        }
+      });
 
       $.ajax({
-        url: "././mail/mail.php",
+        url: "./mail/mail.php",
         type: "POST",
         data: {
-          form: form,
-          name: name,
-          phone: phone,
-          email: email,  
-          message: message
+          form: $.trim(form),
+          name: $.trim(name),
+          phone: $.trim(phone),
+          email: $.trim(email),
+          method: $.trim(method),
+          message: $.trim(message),
+          time: $.trim(time),
+          period: period,
+          material: material,
+          range1: range1,
+          range2: range2,
         },
         cache: false,
-        success: function() {
+        success: function(response) {
           // Success message
           $form.find('.success').html("<div class='alert alert-success'>");
           $form.find('.success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $form.find('.success > .alert-success')
-            .append("<strong>Ваше сообщение успешно отправлено. В ближайшее время наши менеджеры свяжутся с вами! </strong>");
+            .append("<strong>Cообщение успешно отправлено.</strong>");
           $form.find('.success > .alert-success')
             .append('</div>');
-
           // remove prevent submit behaviour and disable preloading
           $form.find("[type=submit]").prop("disabled", false).button('reset');  
-
+          document.location.href="./success.html";
           //clear all fields
           $form.trigger("reset");
         },
@@ -48,29 +73,16 @@ $(function() {
           $form.find('.success').html("<div class='alert alert-danger'>");
           $form.find('.success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $form.find('.success > .alert-danger').append("<strong>Приносим свои извинения, " + firstName + ", но наш почтовый сервер времено не работает. Попробуйте, отправить сообщение еще раз и сообщите нам о проблеме!");
+          $form.find('.success > .alert-danger').append("<strong>Письмо не отправлено. Пожалуйста, проверьте ваше интернет соединения и попробуйте еще раз!");
           $form.find('.success > .alert-danger').append('</div>');
 
           // remove prevent submit behaviour and disable preloading
           $form.find("[type=submit]").prop("disabled", false).button('reset'); 
 
           //clear all fields
-          $form.trigger("reset");
+          //$form.trigger("reset");
         },
-      })
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
-  });
-
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('.success').html('');
+      });
+    }
+  }); 
 });
--- app/mail/mail.php
+++ app/mail/mail.php
@@ -1,105 +1,147 @@
 <?php
+
   if (is_file('lib/class.phpmailer.php')) {
     require_once("lib/class.phpmailer.php");
   }
-
+  
   if (is_file('lib/class.smtp.php')) {
     require_once("lib/class.smtp.php");
   }
+  
+  if (is_file('lib/newsletter.php')) {
+    require_once("lib/newsletter.php");
+  }
 
-  $http_host = $_SERVER['HTTP_HOST'];
+  $http_host = $_SERVER["HTTP_HOST"];
+  $body = "";
+  $post_data = array();
 
-  if ( substr($http_host, 0, 4)=='www.') {
+  if ( substr($http_host, 0, 4)=="www.") {
     $host_name = substr($http_host, 4);
   } else {
     $host_name = $http_host;
   }
-
-  define ('HTTP_SERVER', 'http://' . $http_host . '/');
-  define ('HOST_NAME', $host_name);
-
+  if (isset($_SERVER["HTTP_REFERER"])) {
+    $http_referer = $_SERVER["HTTP_REFERER"];
+  } else {
+    $http_referer = "";
+  }
+  define ("HTTP_SERVER", "http://" . $http_host . "/");
+  define ("HOST_NAME", $host_name);
+  define ("HTTP_REFERER", $http_referer);
   $post = array( 
-    'host_name'     => HOST_NAME,
-    'host_dir'      => HTTP_SERVER,
-    );
+    "host_name"     => HOST_NAME,
+    "host_dir"      => HTTP_SERVER,
+    "host_referer"  => HTTP_REFERER
+  );
 
-  if (!empty($_POST["user_form"])) {
-    $post['user_form'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+  if (!$_POST) {
+    echo "Форма пустая!";
+    exit;
+  }
+  
+  //header("Content-Type: text/html; charset=utf-8");
+  //var_dump($_POST);
+  //exit;
+
+  if ( (!empty($_POST["form"])) && (isset($_POST["form"])) ) {
+    $post["user_form"] = $_POST["form"];
+    $body .= "Форма: " . $post["user_form"] . chr(10) . chr(13);
+    $post_data["Форма:"] = $post["user_form"];
   }
 
-  if (!empty($_POST["email"])) {
-    $post['user_email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  if ( (!empty($_POST["email"])) && (isset($_POST["email"])) ) {
+    $post["user_email"] = $_POST["email"];
+    $body .= "Email: " . $post["user_email"] . chr(10) . chr(13);
+    $post_data["Email:"] = $post["user_email"];
   }
 
-  if (!empty($_POST["name"])) {
-    $post['user_name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+  if ( (!empty($_POST["phone"])) && (isset($_POST["phone"])) ) {
+    $post["user_phone"] = $_POST["phone"];
+    $body .= "Телефон: " . $post["user_phone"] . chr(10) . chr(13);
+    $post_data["Телефон:"] = $post["user_phone"];
   }
 
-  if (!empty($_POST["phone"])) {
-    $post['user_phone'] = filter_input(INPUT_POST,'phone', FILTER_SANITIZE_STRING);
+  if ( (!empty($_POST["name"])) && (isset($_POST["name"])) ) {
+    $post["user_name"] = $_POST["name"];
+    $body .= "Имя: " . $post["user_name"] . chr(10) . chr(13);
+    $post_data["Имя:"] = $post["user_name"];
   }
 
-  if (!empty($_POST["message"])) {
-    $post['user_message'] = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+  if ( (!empty($_POST["message"])) && (isset($_POST["message"])) ) {
+    $post["user_message"] = $_POST["message"];
+    $body .= "Сообщение: " . $post["user_message"] . chr(10) . chr(13);
+    $post_data["Сообщение:"] = $post["user_message"];
   }
 
-  // Insert form data into html
-  $patterns = array();
-  $replacements = array();
-  foreach ($post as $key => $value) {
-    $patterns[] = '/\{%' . $key .  '\%}/i'; // varible => {$varible}
-    $replacements[] = $value;
+  if ( (!empty($_POST["method"])) && (isset($_POST["method"])) ) {
+    $post["user_method"] = $_POST["method"];
+    $body .= "Как связаться: " . $post["user_method"] . chr(10) . chr(13);
+    $post_data["Как связаться:"] = $post["user_method"];
   }
 
-  // html template
-  $body = '';
-  if (is_file('html_template.html')) {
-    $html_template = file_get_contents('html_template.html');
-    $body = preg_replace($patterns, $replacements, $html_template);  
-    $body = preg_replace('/\{%(.+)%\}/', '', $body); // remove all beetween "{% %}"
+  if ( (!empty($_POST["time"])) && (isset($_POST["time"])) ) {
+    $post["user_time"] = $_POST["time"];
+    $body .= "Удобное время: " . $post["user_time"] . chr(10) . chr(13);
+    $post_data["Удобное время:"] = $post["user_time"];
   }
 
-  // If mailer not supported html
-  $altBody = '';
-  if (is_file('no_html.html')) {
-    $no_html_template = file_get_contents('no_html.html');
-    $altBody = preg_replace($patterns, $replacements, $no_html_template);
-    $altBody = preg_replace('/\{%(.+)%\}/', '', $altBody); // remove all beetween "{% %}"
+  if ( !empty($_POST["period"])  && (isset($_POST["period"])) ) {
+    if (is_array($_POST['period'])) {
+      $post["period"] = implode(", ", $_POST["period"]);
+    } else {
+      $post["period"] = $_POST["period"];
+    }
+    $body .= "Когда позвонить: " . $post["period"] . chr(10) . chr(13);
+    $post_data["Когда позвонить:"] = $post["period"];
   }
+
+  if ( !empty($_POST["material"])  && (isset($_POST["material"])) ) {
+    if (is_array($_POST['material'])) {
+      $post["material"] = implode(", ", $_POST["material"]);
+    } else {
+      $post["material"] = $_POST["material"];
+    }
+    $body .= "Чем зашивать: " . $post["material"] . chr(10) . chr(13);
+    $post_data["Чем зашивать:"] = $post["material"];
+  }
+
+  if ( (!empty($_POST["range1"])) && (isset($_POST["range1"])) ) {
+    $post["user_range1"] = $_POST["range1"];
+    $body .= "Длина ворот: " . $post["user_range1"] . chr(10) . chr(13);
+    $post_data["Длина ворот:"] = $post["user_range1"];
+  }
+
+  if ( (!empty($_POST["range2"])) && (isset($_POST["range2"])) ) {
+    $post["user_range2"] = $_POST["range2"];
+    $body .= "Высота ворот: " . $post["user_range2"] . chr(10) . chr(13);
+    $post_data["Высота ворот:"] = $post["user_range2"];
+  }
+
+  $body .= "Форма отправлена с сайта: " . $post["host_referer"];
+  $post_data["Форма отправлена с сайта:"] = $post["host_referer"];
+
   $mail = new PHPMailer();
-
-  $mail->CharSet      = 'UTF-8';
-
-  //if mail is SMTP
-  /*
-  $mail->isSMTP();
-  $mail->Host         = 'smtp.server.com';
-  $mail->SMTPAuth     = true;
-  $mail->SMTPSecure   = 'ssl';
-  $mail->Port         = 465;
-  $mail->Username     = 'name@mail.com';
-  $mail->Password     = 'password';
-  */
-
+  $mail->CharSet = "UTF-8";
   $mail->IsSendmail();
 
-  $from = 'no-reply@tagopen.com';
-  $to = "Artem2431@gmail.com";
+  $from = "no-repeat@" . HOST_NAME;
   $mail->SetFrom($from, HOST_NAME);
-  $mail->AddAddress($to, 'Name Surname');
-
-  $mail->isHTML(false);
-
-  $mail->Subject      = "Новая заявка с сайта";
-  $mail->Body         = $body;
-  $mail->AltBody      = $altBody;
+  $mail->AddAddress("Artem2431@gmail.com");
+  $mail->AddAddress("Marchik88@rambler.ru");
+  $mail->isHTML(true);
+  $mail->Subject      = HOST_NAME;
+  $NewsLetterClass    = new NewsLetterClass();
+  $mail->Body         = $NewsLetterClass->generateHTMLLetter($post_data);
+  $mail->AltBody      = $body;
 
   if(!$mail->send()) {
-    echo 'Что-то пошло не так. ' . $mail->ErrorInfo;
+    echo "Что-то пошло не так. " . $mail->ErrorInfo;
     return false;
   } else {
-    echo 'Сообщение отправлено';
+    header("Location: ../success.html");
     return true;
   }
-
 ?>
