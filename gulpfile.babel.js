e328503f6f949d48b25b709019a25df750491e3e
Artem Shcherbakov <Artem2431@gmail.com>
2017-11-06 07:04:44 +0200

    update to bootstrap.4.0.0-beta2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -75,7 +75,7 @@ gulp.task('sass', () => {
 
 gulp.task('scripts', () => {
   return gulp.src([
-      'node_modules/tether/dist/js/tether.js',
+      'node_modules/popper.js/dist/umd/popper.js',
       'node_modules/bootstrap/dist/js/bootstrap.js',
       'node_modules/jquery/dist/jquery.js',
       'node_modules/bootstrap-validator/dist/validator.js',
