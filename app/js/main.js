9ed5a3bc3671f39d3700df4f6f0f36a774e85cc5
Security2431 <Artem2431@gmail.com>
2018-05-11 11:57:48 +0300

    add to basicElements wowjs

--- app/js/main.js
+++ app/js/main.js
@@ -390,6 +390,15 @@ const GRVE = GRVE || {};
         })
       })
     },
+    wowjs() {
+      var wow = new WOW({
+        boxClass:     'js-wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       false       // trigger animations on mobile devices (true is default)
+      })
+      wow.init()
+    },
     countdown() {
       $('[data-countdown]').each(function() {
         const $this =                $(this)
